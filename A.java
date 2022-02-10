eb8765091a9201976108c7e5ccf3d250b7664c03
unknown <simiyon2807@gmail.com>
2022-02-10 21:36:46 +0530

    first commit

---
+++ A.java
@@ -0,0 +1,68 @@
+package org.gan;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class A {
+
+	public static void main(String[] args) {
+
+		Sam s = new Sam();
+		s.setEmpId(100);
+		s.setEmpName("Ajay");
+		s.setEmpNumber(9876543210l);
+
+		Sam s1 = new Sam();
+		s1.setEmpId(200);
+		s1.setEmpName("Arun");
+		s1.setEmpNumber(8907654321l);
+
+		Sam s2 = new Sam();
+		s2.setEmpId(300);
+		s2.setEmpName("Arjun");
+		s2.setEmpNumber(976532190l);
+//User defined List
+		List<Sam> li = new ArrayList<>();
+		li.add(s);
+		li.add(s1);
+		li.add(s2);
+
+		for (int i = 0; i < li.size(); i++) {
+			System.out.println(li.get(i).getEmpId());
+			System.out.println(li.get(i).getEmpName());
+			System.out.println(li.get(i).getEmpNumber());
+		}
+		// User defined Set
+		Set<Sam> si = new LinkedHashSet<>();
+		si.add(s);
+		si.add(s1);
+		si.add(s2);
+
+		for (Sam sam : si) {
+			System.out.println(sam.getEmpId());
+			System.out.println(sam.getEmpName());
+			System.out.println(sam.getEmpNumber());
+		}
+
+		// user defined Map
+
+		Map<Sam, Integer> mp = new LinkedHashMap<>();
+		mp.put(s, 1);
+		mp.put(s1, 2);
+		mp.put(s2, 3);
+
+		Set<Entry<Sam, Integer>> entrySet = mp.entrySet();
+		for (Entry<Sam, Integer> entry : entrySet) {
+			System.out.println(entry.getKey().getEmpId());
+			System.out.println(entry.getKey().getEmpNumber());
+			System.out.println(entry.getKey().getEmpName());
+		}
+
+	}
+
+}
---
+++ Sam.java
@@ -0,0 +1,38 @@
+package org.gan;
+
+public class Sam {
+
+	private int empId;
+
+	private String empName;
+
+	private long empNumber;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public long getEmpNumber() {
+		return empNumber;
+	}
+
+	public void setEmpNumber(long empNumber) {
+		this.empNumber = empNumber;
+	}
+
+	
+	
+	
+}
